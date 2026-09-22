b2c6e84a369b226e166f53d96b5e268bc6671dba
Onyx_i7 <Crackdroid69@gmail.com>
2026-09-21 22:56:47 -0400

    Fixed unusable item

--- src/main/java/com/invadermonky/villagercontracts/client/gui/GuiVillagerContracts.java
+++ src/main/java/com/invadermonky/villagercontracts/client/gui/GuiVillagerContracts.java
@@ -5,6 +5,7 @@ import com.invadermonky.villagercontracts.compat.GameStageIntegration;
 import com.invadermonky.villagercontracts.handlers.ConfigHandler;
 import com.invadermonky.villagercontracts.handlers.ConfigHandler.ContractCostType;
 import com.invadermonky.villagercontracts.handlers.EventHandler;
+import com.invadermonky.villagercontracts.network.PacketApplyContractName;
 import com.invadermonky.villagercontracts.util.VillagerDataHelper;
 import com.invadermonky.villagercontracts.util.VillagerInfo;
 import net.minecraft.client.gui.GuiScreen;
@@ -540,7 +541,8 @@ public class GuiVillagerContracts extends GuiScreen {
                     TextFormatting.RED + I18n.format("gui.villagercontracts.no_contract")));
             return;
         }
-
+		
+		VillagerContracts.NETWORK.sendToServer(new PacketApplyContractName(name));
         this.mc.player.closeScreen();
     }
 
